938b7e826742d4563da1a3b413cc729fe543d785
kirti-italiya <kirtiitaliya.depthin@gmail.com>
2023-05-02 12:37:23 +0530

    main change

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Create a new repository on the command line</p>
         <a
           className="App-link"
           href="https://reactjs.org"
